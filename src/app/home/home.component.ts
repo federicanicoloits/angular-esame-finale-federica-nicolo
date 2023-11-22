e052697ac09dee94f47d8e58e4e1e46da8b25dcd
federicanicoloits <127236809+federicanicoloits@users.noreply.github.com>
2023-11-22 19:09:12 +0100

    final (maybe)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   lat = '';
   lon = '';
+  InputValidation(): boolean {
+    const latPattern = /^(-?\d{1,2}(?:\.\d{1,6})?|\b90(?:\.0{1,6})?)$/;
+    const longPattern = /^(-?\d{1,3}(?:\.\d{1,6})?|\b180(?:\.0{1,6})?)$/;
+    return latPattern.test(this.lat) && longPattern.test(this.lon);
+  }
 }
